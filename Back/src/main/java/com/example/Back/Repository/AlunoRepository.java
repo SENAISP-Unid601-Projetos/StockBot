86b7632f9fd96ee1985e2cb09edc86a831734d87
WesleyC115 <wesley.moura03@hotmail.com>
2025-05-23 17:27:04 -0300

    Classes Aluno

--- Back/src/main/java/com/example/Back/Repository/AlunoRepository.java
+++ Back/src/main/java/com/example/Back/Repository/AlunoRepository.java
@@ -3,6 +3,9 @@ package com.example.Back.Repository;
 import com.example.Back.Entity.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+import java.util.List;
 
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    List<Aluno> findByNomeContainingIgnoreCase(String nome);
 }
+
