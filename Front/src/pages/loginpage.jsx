ae036469365e7ae494ab41b52233a29cdad7da80
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-09-25 15:01:34 -0300

    atualização no domínio

--- Front/src/pages/loginpage.jsx
+++ Front/src/pages/loginpage.jsx
@@ -1,25 +1,29 @@
+/*
+* File: senaisp-unid601-projetos/stockbot/StockBot-19a9513533a1e735515fdc76ec3de9e98b02cee9/Front/src/pages/loginpage.jsx
+*/
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios'; // Usa o axios normal
 import './loginpage.css';
 
-const apiUrl = 'http://localhost:8080/api/auth';
+const apiUrl = 'http://localhost:8081/api/auth';
 
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [dominioEmpresa, setDominioEmpresa] = useState(''); // Estado para o domínio da empresa
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post(`${apiUrl}/login`, { email, senha });
+      const response = await axios.post(`${apiUrl}/login`, { email, senha, dominioEmpresa });
       const token = response.data.token;
       localStorage.setItem('jwt-token', token);
       navigate('/');
     } catch (error) {
       console.error('Erro de login:', error);
-      alert('E-mail ou senha inválidos.');
+      alert('Credenciais inválidas. Verifique o e-mail, senha e domínio.');
     }
   };
 
@@ -28,6 +32,14 @@ function LoginPage() {
       <div className="form-wrapper">
         <form id="login-form" onSubmit={handleLogin}>
           <h2>Acessar o StockBot</h2>
+          <label htmlFor="dominio">Domínio da Empresa</label>
+          <input 
+            type="text" 
+            id="dominio"
+            value={dominioEmpresa}
+            onChange={(e) => setDominioEmpresa(e.target.value)}
+            required 
+          />
           <label htmlFor="email">E-mail</label>
           <input 
             type="email" 
